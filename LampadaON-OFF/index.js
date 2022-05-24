72b452f0524066ace5a0ad9dd5eb81631e7f8aca
vinicius evandro <vinnievandro@hotmail.com>
2022-05-24 17:15:24 -0300

    att

---
+++ LampadaON-OFF/index.js
@@ -0,0 +1,13 @@
+
+ligarDesligar = document.getElementById()
+
+ligarDesligar.addEventListener('click', () => {
+
+    re
+
+});
+
+document.querySelector('.show-more').addEventListener('click', function () {
+    document.querySelector('.additional-information')
+      ('is-visible');
+  });
